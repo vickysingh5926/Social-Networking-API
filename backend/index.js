213434dc84ff8801230ff02a93078807ea8b8157
Vicky Singh <vickysingh88050@gmail.com>
2024-03-10 16:01:12 +0530

    Final Commit

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,10 @@ app.use(express.json());
 app.use("/auth", require("./Routes/auth.route"));
 app.use("/post", require("./Routes/post.route"));
 
+app.get("/",(req,res)=>{
+  res.status(200).send("Please Read API DOCS from https://vicky.gitbook.io/social-networking-api-1  or  POSTMAN DOCS From https://documenter.getpostman.com/view/24931735/2sA2xh3D3a");
+})
+
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
